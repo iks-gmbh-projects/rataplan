fbcd8b1b8dcd862a115aead2d5f92744a4a25f38
HartlIKS <T.Hartl@iks-gmbh.com>
2022-09-28 15:56:22 +0200

    haupteinsicht in eigene Umfragen

--- rataplan-frontend/src/app/survey/survey-view/survey-view.component.ts
+++ rataplan-frontend/src/app/survey/survey-view/survey-view.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Survey } from '../survey.model';
 
 @Component({
   selector: 'app-survey-view',
   templateUrl: './survey-view.component.html',
   styleUrls: ['./survey-view.component.css']
 })
-export class SurveyViewComponent implements OnInit {
+export class SurveyViewComponent implements OnInit, OnDestroy {
+  public survey: Survey|null = null;
 
-  constructor() { }
+  private sub: Subscription|null = null;
 
-  ngOnInit(): void {
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  public ngOnInit(): void {
+    this.survey = this.route.snapshot.data['survey'];
+    this.sub = this.route.data.subscribe(d => this.survey = d['survey']);
+  }
+
+  public ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  public ensureDate(input: Date|number|string): string {
+    let ret = new Date(input).toISOString().split('T')[0];
+    return ret;
   }
 
 }
