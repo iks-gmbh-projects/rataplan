e3885f81adbf9f6a10c20ed7a5f3c37dca92fb14
HartlIKS <T.Hartl@iks-gmbh.com>
2022-10-07 15:22:53 +0200

    fix für nicht funktionierended button im preview

--- rataplan-frontend/src/app/survey/survey-form/page/page.component.ts
+++ rataplan-frontend/src/app/survey/survey-form/page/page.component.ts
@@ -9,10 +9,11 @@ import { Answer, Checkbox, QuestionGroup } from '../../survey.model';
 })
 export class PageComponent {
   @Input() public questionGroup?: QuestionGroup;
+  @Input() public preview: boolean = false;
   @Output() public readonly onSubmit = new EventEmitter<{ [key: string | number]: Answer }>();
 
   public submit(form: NgForm) {
-    if (form.valid) {
+    if (this.preview || form.valid) {
       let answers: {[key: string|number]: Answer&{checkboxId?: string|number}} = form.value;
       for(let key in answers) {
         if(answers[key].checkboxId) {
@@ -24,7 +25,6 @@ export class PageComponent {
         }
       }
       this.onSubmit.emit(answers);
-      form.resetForm();
     }
   }
 
