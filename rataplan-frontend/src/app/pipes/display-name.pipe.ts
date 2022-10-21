1551f0e01cc10bf39fe8056ec76ad33d96131087
HartlIKS <T.Hartl@iks-gmbh.com>
2022-10-21 10:11:12 +0200

    import organization

--- rataplan-frontend/src/app/pipes/display-name.pipe.ts
+++ rataplan-frontend/src/app/pipes/display-name.pipe.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Pipe, PipeTransform } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 import { DisplayNameService } from '../services/displayname-service/displayname.service';
 
 @Pipe({
--- rataplan-frontend/src/app/survey/survey.module.ts
+++ rataplan-frontend/src/app/survey/survey.module.ts
@@ -1,5 +1,4 @@
 import { ClipboardModule } from '@angular/cdk/clipboard';
-import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
