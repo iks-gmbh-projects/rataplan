1b5b1f0588e7f07486cc1f017e094bb8fe557bd3
dayIKS <D.Aydin@iks-gmbh.com>
2022-10-24 11:22:59 +0200

    changed environment.prod.ts with additional URLs

--- rataplan-frontend/src/environments/environment.prod.ts
+++ rataplan-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  surveyBackendURL: "https://umfragetool-backend.herokuapp.com/",
+  surveyBackendURL: 'https://umfragetool-backend.herokuapp.com/',
+  rataplanBackendURL: 'https://rataplan-backend-test.herokuapp.com/v1',
+  authBackendURL: 'https://rataplan-auth.herokuapp.com/v1'
   // TODO add publically accessible backend URLs once they exist
 };
