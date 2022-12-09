1a14e9f2c60c9f7857d28146bcb84c812fd41181
kwi <kai.wincek@gmail.com>
2022-12-09 12:18:46 +0100

    update userid values

--- rataplan-backend/src/main/resources/migration/prod/V13__REMOVE_BACKENDUSER.sql
+++ rataplan-backend/src/main/resources/migration/prod/V13__REMOVE_BACKENDUSER.sql
@@ -1,21 +1,34 @@
 ALTER TABLE appointmentmember
-DROP CONSTRAINT backendUserId;
+DROP CONSTRAINT appointmentmember_backenduserid_fkey;
 
 ALTER TABLE appointmentmember
 RENAME COLUMN backenduserid TO userid;
 
-INSERT INTO appointmentmember (userid)
-VALUES
+UPDATE appointmentmember SET appointmentmember.userid=backenduser.authuserid
+FROM appointmentmember
+WHERE appointmentmember.backenduserid=backenduser.id
+JOIN backenduser on (appointmentmember.userid=backenduser.authuserid);
 
 ALTER TABLE appointmentrequest
-DROP CONSTRAINT backendUserId;
+DROP CONSTRAINT appointmentrequest_backenduserid_fkey;
 
 ALTER TABLE appointmentrequest
 RENAME COLUMN backenduserid TO userid;
 
+UPDATE appointmentrequest
+SET appointmentrequest.userid=backenduser.authuserid
+FROM appointmentrequest
+JOIN backenduser on (appointmentrequest.userid=backenduser.authuserid);
+
 ALTER TABLE backenduseraccess
-DROP CONSTRAINT backendUserId;
+DROP CONSTRAINT backenduseraccess_backenduserid_fkey;
 
 ALTER TABLE backenduseraccess
 RENAME COLUMN backenduserid TO userid;
 
+UPDATE backenduseraccess
+SET backenduseraccess.userid=backenduser.authuserid
+FROM backenduseraccess
+JOIN backenduser on (appointmentmember.userid=backenduser.authuserid);
+
+DROP TABLE backenduser;
