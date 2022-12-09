7355b39518d499921a472787b2ddf2dff20376df
day <D.Aydin@iks-gmbh.com>
2022-12-09 15:57:34 +0100

    added vote-list to see all created and participated votes

---
+++ rataplan-frontend/src/app/vote-list/vote-list.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { AppointmentRequestModel } from '../models/appointment-request.model';
+import { DashboardService } from '../services/dashboard-service/dashboard.service';
+
+@Component({
+  selector: 'app-survey-list',
+  templateUrl: './vote-list.component.html',
+  styleUrls: ['./vote-list.component.css']
+})
+export class VoteListComponent implements OnInit, OnDestroy {
+  // public surveys: SurveyHead[] = [];
+  public createdVotes: AppointmentRequestModel[] = [];
+  public participatedVotes: AppointmentRequestModel[] = [];
+  public busy = false;
+  public error: any = null;
+  public isOwn = false;
+  private sub?: Subscription;
+
+  constructor(private dashboardService: DashboardService, private activeRoute: ActivatedRoute) { }
+
+  public ngOnInit(): void {
+    // this.isOwn = this.activeRoute.snapshot.data['own'];
+    this.updateList();
+    // this.sub = this.activeRoute.data.subscribe(data => {
+    //   this.isOwn = data['own'];
+    //   this.updateList();
+    // });
+  }
+
+  public ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  public updateList(): void {
+    // if (this.busy) return;
+    // this.busy = true;
+    // this.error = null;
+    // const request = this.isOwn ? this.surveyService.getOwnSurveys() : this.surveyService.getOpenSurveys();
+    // request.subscribe({
+    //   next: s => this.surveys = s,
+    //   error: err => {
+    //     this.error = err;
+    //     this.busy = false;
+    //   },
+    //   complete: () => this.busy = false,
+    // });
+
+    this.dashboardService.getCreatedVotes().subscribe(res => {
+      this.createdVotes = res;
+    });
+
+    this.dashboardService.getParticipatedVotes().subscribe(res => {
+      this.participatedVotes = res;
+    });
+  }
+}
