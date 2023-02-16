5359d8c0cf5ce5a8f91fe25b1df58c065f2016d8
HartlIKS <T.Hartl@iks-gmbh.com>
2023-02-16 08:46:55 +0100

    Remove errant "is-what" dependency

--- rataplan-frontend/src/app/app.component.ts
+++ rataplan-frontend/src/app/app.component.ts
@@ -1,21 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from "./services/login.service/login.service";
-import { isUndefined, isNullOrUndefined } from 'is-what';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements OnInit{
-  title = 'drumdibum-rework';
+export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.loginService.getUserData()
   }
 
   constructor(private loginService: LoginService) {
-    if (!isNullOrUndefined(localStorage.getItem('username'))) {
+    if (localStorage.getItem('username')) {
       loginService.getUserData().subscribe(res => {
 
 
