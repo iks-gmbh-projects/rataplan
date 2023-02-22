3f29bb20b46b6a28f9f7ee6e897cffe551e820d2
HartlIKS <T.Hartl@iks-gmbh.com>
2023-02-22 08:16:26 +0100

    fix vote-list service

--- rataplan-frontend/src/app/services/dashboard-service/vote-list.service.ts
+++ rataplan-frontend/src/app/services/dashboard-service/vote-list.service.ts
@@ -1,23 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { environment } from '../../../environments/environment';
+import { BackendUrlService } from "../backend-url-service/backend-url.service";
+import { exhaustMap, Observable } from "rxjs";
+import { AppointmentRequestModel } from "../../models/appointment-request.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteListService {
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private urlService: BackendUrlService
+  ) {
   }
 
-  public getParticipatedVotes(){
-    const url = environment.rataplanBackendURL+'users/appointmentRequests/participations';
-    return this.http.get<any>(url, { withCredentials: true });
+  public getParticipatedVotes(): Observable<AppointmentRequestModel[]> {
+    return this.urlService.appointmentURL$.pipe(
+      exhaustMap(url => this.http.get<AppointmentRequestModel[]>(url+'users/appointmentRequests/participations', { withCredentials: true }))
+    );
   }
 
-  public getCreatedVotes(){
-    const url = environment.rataplanBackendURL+'users/appointmentRequests/creations';
-    return this.http.get<any>(url, { withCredentials: true });
+  public getCreatedVotes(): Observable<AppointmentRequestModel[]> {
+    return this.urlService.appointmentURL$.pipe(
+      exhaustMap(url => this.http.get<AppointmentRequestModel[]>(url+'users/appointmentRequests/creations', { withCredentials: true }))
+    );
   }
 }
