fbaf06ea10ce5e38215ddfa4d29a6d4a453372f6
HartlIKS <T.Hartl@iks-gmbh.com>
2023-03-10 08:14:56 +0100

    Apply encryption job performance improvements from surveytool refactor

--- rataplan-backend/src/main/java/de/iks/rataplan/repository/AppointmentRepository.java
+++ rataplan-backend/src/main/java/de/iks/rataplan/repository/AppointmentRepository.java
@@ -1,9 +1,14 @@
 package de.iks.rataplan.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import de.iks.rataplan.domain.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
-
+    @Query(value = "SELECT * FROM appointment" +
+        " WHERE (description IS NOT NULL AND description NOT LIKE 'ENC\\_\\_##\\_\\_%')" +
+        " OR (url IS NOT NULL AND url NOT LIKE 'ENC\\_\\_##\\_\\_%')", nativeQuery = true)
+    public Stream<Appointment> findUnencrypted();
 }
