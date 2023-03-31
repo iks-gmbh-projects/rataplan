8a567a5812417ae7d3cea3a12e5f410e4fd8b4c1
day <D.Aydin@iks-gmbh.com>
2023-03-31 13:24:16 +0200

    added test cases for user who takes twice at an appointmentRequest

--- rataplan-backend/src/integration/java/de/iks/rataplan/config/IntegrationConfig.java
+++ rataplan-backend/src/integration/java/de/iks/rataplan/config/IntegrationConfig.java
@@ -6,6 +6,11 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+import de.iks.rataplan.mapping.DecisionConverter;
+import de.iks.rataplan.mapping.crypto.FromEncryptedStringConverter;
+import de.iks.rataplan.mapping.crypto.ToEncryptedStringConverter;
+import de.iks.rataplan.service.MockCryptoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -19,9 +24,26 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 @PropertySource("classpath:integration.properties")
 public class IntegrationConfig {
 
+    @Autowired
+    private DecisionConverter decisionConverter;
+
+    private final MockCryptoService mockCryptoService = new MockCryptoService();
+    private final FromEncryptedStringConverter fromEncryptedStringConverter = new FromEncryptedStringConverter(mockCryptoService);
+    private final ToEncryptedStringConverter toEncryptedStringConverter = new ToEncryptedStringConverter(mockCryptoService);
+
 	@Autowired
 	Environment environment;
-	
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.addConverter(decisionConverter.toDAO);
+        mapper.addConverter(decisionConverter.toDTO);
+        mapper.addConverter(toEncryptedStringConverter);
+        mapper.addConverter(fromEncryptedStringConverter);
+        return mapper;
+    }
+
     @Bean
     public DataSource dataSource() {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
