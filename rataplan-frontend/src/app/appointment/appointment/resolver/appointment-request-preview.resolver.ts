d7ba63f401c219c6540184ccc4957b9d2d7c072a
HartlIKS <T.Hartl@iks-gmbh.com>
2023-04-21 10:38:26 +0200

    Weiterleitung zum GeneralSubformComponent, wenn der Benutzer die URL der Vorschauseite manuell in den Browser eingibt.

--- rataplan-frontend/src/app/appointment/appointment/resolver/appointment-request-preview.resolver.ts
+++ rataplan-frontend/src/app/appointment/appointment/resolver/appointment-request-preview.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
-import { filter, map, Observable, take } from 'rxjs';
+import { catchError, EMPTY, filter, map, Observable, take, timeout } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { AppointmentRequestModel } from '../../../models/appointment-request.model';
@@ -17,12 +17,19 @@ export class AppointmentRequestPreviewResolver implements Resolve<AppointmentReq
   ) {
   }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<AppointmentRequestModel> | Promise<AppointmentRequestModel> | AppointmentRequestModel {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<AppointmentRequestModel> {
     return this.store.select('appointmentRequest')
       .pipe(
         filter(state => state.complete!),
         map(state => state.appointmentRequest!),
         take(1),
+        timeout(100),
+        catchError(() => {
+          const editToken = route.parent!.params['id'];
+          if(editToken) this.router.navigate(['/vote/edit', editToken]);
+          else this.router.navigate(['/create-vote']);
+          return EMPTY;
+        }),
       );
   }
 }
