31efc6e8d12591da98ae14b31860213ab4c5a7a4
HartlIKS <T.Hartl@iks-gmbh.com>
2023-04-26 10:48:01 +0200

    Fixed ValueChanged error on load in SurveyForm.

--- rataplan-frontend/src/app/survey/survey-form/survey-form.component.ts
+++ rataplan-frontend/src/app/survey/survey-form/survey-form.component.ts
@@ -1,29 +1,53 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatStepper } from '@angular/material/stepper';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { combineLatest, map, Observable, ReplaySubject, startWith, Subscription, switchMap } from 'rxjs';
 import { Answer, Survey } from '../survey.model';
 import { SurveyService } from '../survey.service';
 import { SurveyAnswerComponent } from './survey-answer/survey-answer.component';
+import { PageComponent } from './page/page.component';
 
 @Component({
   selector: 'app-survey-form',
   templateUrl: './survey-form.component.html',
-  styleUrls: ['./survey-form.component.css']
+  styleUrls: ['./survey-form.component.css'],
 })
 
 export class SurveyFormComponent implements OnInit, OnDestroy {
   public survey?: Survey;
-  private answers: {[key: string|number]:Answer} = {};
+  private answers: { [key: string | number]: Answer } = {};
   private sub?: Subscription;
-  constructor(private route: ActivatedRoute, private router:Router, private surveys:SurveyService, private snackBars:MatSnackBar, private dialogs:MatDialog) { }
+  private readonly pagesSubject = new ReplaySubject<QueryList<PageComponent>>(1);
+  readonly isResponseValid: Observable<boolean> = this.pagesSubject.pipe(
+    switchMap(pages => pages.changes.pipe(
+      map(() => pages),
+      startWith(pages),
+      switchMap(pages => combineLatest(
+        pages.map(
+          page => page.form!.statusChanges!.pipe(
+            map(() => page.form!.invalid),
+            startWith(page.form!.invalid)
+          )
+        ),
+      )),
+      map(values => values.some(v => v))
+    )),
+    startWith(false)
+  );
+
+  @ViewChildren("surveyPage") set pages(pages: QueryList<PageComponent>) {
+    this.pagesSubject.next(pages);
+  }
+
+  constructor(private route: ActivatedRoute, private router: Router, private surveys: SurveyService, private snackBars: MatSnackBar, private dialogs: MatDialog) {
+  }
 
   public ngOnInit(): void {
     this.survey = this.route.snapshot.data['survey'];
     this.sub = this.route.data.subscribe(data => {
-      if(this.survey !== data['survey']) {
+      if (this.survey !== data['survey']) {
         this.survey = data['survey'];
         this.answers = {};
       }
@@ -34,9 +58,9 @@ export class SurveyFormComponent implements OnInit, OnDestroy {
     this.sub?.unsubscribe();
   }
 
-  public pageSubmit(stepper: MatStepper, answers?: {[key: string|number]: Answer}) {
-    if(!this.survey) return;
-    if(answers) {
+  public pageSubmit(stepper: MatStepper, answers?: { [key: string | number]: Answer }) {
+    if (!this.survey) return;
+    if (answers) {
       this.answers = {...this.answers, ...answers};
       stepper.next();
     } else {
@@ -44,13 +68,9 @@ export class SurveyFormComponent implements OnInit, OnDestroy {
     }
   }
 
-  public isInvalid(stepper: MatStepper): boolean {
-    return stepper.steps.reduce<boolean>((invalid, step) => invalid || step.hasError, false);
-  }
-
   public submit(stepper: MatStepper): void {
-    if(!this.survey) return;
-    if(stepper.selectedIndex >= this.survey.questionGroups.length) {
+    if (!this.survey) return;
+    if (stepper.selectedIndex >= this.survey.questionGroups.length) {
       this.surveys.answerSurvey({
         surveyId: this.survey.id!,
         answers: this.answers,
