816c3a7b54b340b8a15c48af9075bc11901fc7b1
HartlIKS <T.Hartl@iks-gmbh.com>
2023-05-25 11:57:08 +0200

    Adjust flyway scripts to elongate encrypted text fields

--- surveyTool/src/main/resources/db/migration/V3.1__Add_Encryption_Prefix.sql
+++ surveyTool/src/main/resources/db/migration/V3.1__Add_Encryption_Prefix.sql
@@ -1,18 +1,36 @@
+ALTER TABLE answer
+ALTER COLUMN text SET DATA TYPE VARCHAR(2000);
+
 UPDATE answer
 SET text = 'RAW__##__' || text
 WHERE text IS NOT NULL;
 
+ALTER TABLE checkbox
+ALTER COLUMN text SET DATA TYPE VARCHAR(400);
+
 UPDATE checkbox
 SET text = 'RAW__##__' || text;
+
+ALTER TABLE question
+ALTER COLUMN text SET DATA TYPE VARCHAR(400);
 
 UPDATE question
 SET text = 'RAW__##__' || text;
 
+ALTER TABLE questionGroup
+ALTER COLUMN title SET DATA TYPE VARCHAR(400);
+
 UPDATE questionGroup
 SET title = 'RAW__##__' || title;
 
+ALTER TABLE survey
+ALTER COLUMN name SET DATA TYPE VARCHAR(400);
+
 UPDATE survey
 SET name = 'RAW__##__' || name;
+
+ALTER TABLE survey
+ALTER COLUMN description SET DATA TYPE VARCHAR(4000);
 
 UPDATE survey
 SET description = 'RAW__##__' || description
