c85150f81235eb051f1cb824816260457309ec29
HartlIKS <T.Hartl@iks-gmbh.com>
2023-06-29 14:10:39 +0200

    Add hidden endpoint to fetch a user id by email address

--- rataplan-auth/src/main/java/de/iks/rataplan/controller/RataplanAuthCommController.java
+++ rataplan-auth/src/main/java/de/iks/rataplan/controller/RataplanAuthCommController.java
@@ -1,9 +1,13 @@
 package de.iks.rataplan.controller;
 
+import de.iks.rataplan.domain.User;
 import de.iks.rataplan.dto.KeyDTO;
 import de.iks.rataplan.service.CryptoService;
+import de.iks.rataplan.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.PublicKey;
@@ -12,9 +16,16 @@ import java.security.PublicKey;
 @RequiredArgsConstructor
 public class RataplanAuthCommController {
     private final CryptoService cryptoService;
+    private final UserService userService;
     @GetMapping("/pubid")
     public KeyDTO getIDKey() {
         PublicKey idKey = cryptoService.idKey();
         return new KeyDTO(idKey.getAlgorithm(), idKey.getEncoded());
+    }
+    @GetMapping("/userid")
+    public ResponseEntity<?> getIdByEmail(@RequestParam String email) {
+        User user = userService.getUserFromEmail(email);
+        if(user == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user.getId());
     }
 }
