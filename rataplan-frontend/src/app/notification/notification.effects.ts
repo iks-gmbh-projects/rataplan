ccfe0c50102b6fc17f1763e3044f66f515d575fc
HartlIKS <T.Hartl@iks-gmbh.com>
2023-07-03 10:28:01 +0200

    Move vote notification loading to main app module, as the voteModule isn't loaded early enough.

--- rataplan-frontend/src/app/notification/notification.effects.ts
+++ rataplan-frontend/src/app/notification/notification.effects.ts
@@ -1,8 +1,14 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { notificationActions } from './notification.actions';
-import { filter, tap } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { authFeature } from '../authentication/auth.feature';
+import { of, sample, startWith, switchMap } from 'rxjs';
+import { AuthActions } from '../authentication/auth.actions';
+import { voteNotificationtypes } from '../vote/vote.notificationtypes';
+import { Store } from '@ngrx/store';
+import { VoteListService } from '../services/dashboard-service/vote-list.service';
 
 @Injectable({
   providedIn: "root",
@@ -10,7 +16,9 @@ import { Injectable } from '@angular/core';
 export class NotificationEffects {
   constructor(
     private $actions: Actions,
+    private store: Store,
     private snackBars: MatSnackBar,
+    private voteListService: VoteListService,
   ) {
   }
 
@@ -18,7 +26,30 @@ export class NotificationEffects {
     return this.$actions.pipe(
       ofType(notificationActions.notify),
       filter(notif => !!notif.message),
-      tap(notif => this.snackBars.open(notif.message!, "OK"))
+      tap(notif => this.snackBars.open(notif.message!, "OK")),
     );
-  }, {dispatch: false});
+  }, { dispatch: false });
+
+  clearOnLogout = createEffect(() => {
+    return this.$actions.pipe(
+      ofType(AuthActions.LOGOUT_ACTION),
+      map(() => notificationActions.clearall()),
+    );
+  });
+
+  loadVoteNotifications = createEffect(() => {
+    return this.store.select(authFeature.selectUser).pipe(
+      sample(this.$actions.pipe(
+        ofType(AuthActions.LOGIN_SUCCESS_ACTION),
+        startWith(null),
+      )),
+      filter(u => !!u),
+      switchMap(() => this.voteListService.getCondignedVotes()),
+      map(v => v.length),
+      switchMap(n => of(
+        notificationActions.clear(voteNotificationtypes.consigns),
+        notificationActions.notify(voteNotificationtypes.consigns, n),
+      )),
+    );
+  });
 }
--- rataplan-frontend/src/app/vote/vote.effects.ts
+++ rataplan-frontend/src/app/vote/vote.effects.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Actions, concatLatestFrom, createEffect, ofType, ROOT_EFFECTS_INIT } from '@ngrx/effects';
+import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { catchError, delayWhen, from, of, startWith, switchMap, take } from 'rxjs';
+import { catchError, delayWhen, from, of, switchMap, take } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { deserializeVoteModel, VoteModel } from '../models/vote.model';
 import { BackendUrlService } from '../services/backend-url-service/backend-url.service';
@@ -18,9 +18,6 @@ import {
 import { DecisionType } from './vote-form/decision-type.enum';
 import { voteFeature } from './vote.feature';
 import { authFeature } from '../authentication/auth.feature';
-import { VoteListService } from '../services/dashboard-service/vote-list.service';
-import { notificationActions } from '../notification/notification.actions';
-import { voteNotificationtypes } from './vote.notificationtypes';
 
 @Injectable({
   providedIn: 'root',
@@ -33,91 +30,86 @@ export class VoteEffects {
     private readonly router: Router,
     private readonly activeRoute: ActivatedRoute,
     private readonly urlService: BackendUrlService,
-    private readonly voteListService: VoteListService
   ) {
   }
 
-  loadNotifications = createEffect(() => {
+  initVote = createEffect(() => {
     return this.actions$.pipe(
-      ofType(ROOT_EFFECTS_INIT),
-      startWith(null),
-      switchMap(() => this.voteListService.getCondignedVotes()),
-      map(v => v.length),
-      switchMap(n => of(
-        notificationActions.clear(voteNotificationtypes.consigns),
-        notificationActions.notify(voteNotificationtypes.consigns, n)
-      ))
+      ofType(VoteActions.INIT),
+      switchMap((action: InitVoteAction) => {
+        if(!action.id) return of(new InitVoteSuccessAction({
+          title: '',
+          deadline: '',
+          voteConfig: {
+            voteOptionConfig: {
+              startDate: true,
+              startTime: false,
+              endDate: false,
+              endTime: false,
+              description: false,
+              url: false,
+            },
+            decisionType: DecisionType.DEFAULT,
+          },
+          options: [],
+          participants: [],
+          consigneeList: [],
+        }));
+        else return this.urlService.voteURL$.pipe(
+          switchMap(url => this.http.get<VoteModel<true>>(url + '/votes/edit/' + action.id, { withCredentials: true })),
+          map(deserializeVoteModel),
+          map(request => new InitVoteSuccessAction(request)),
+          catchError(err => of(new InitVoteErrorAction(err))),
+        );
+      }),
     );
   });
 
-  initVote = createEffect(() => { return this.actions$.pipe(
-    ofType(VoteActions.INIT),
-    switchMap((action: InitVoteAction) => {
-      if (!action.id) return of(new InitVoteSuccessAction({
-        title: '',
-        deadline: '',
-        voteConfig: {
-          voteOptionConfig: {
-            startDate: true,
-            startTime: false,
-            endDate: false,
-            endTime: false,
-            description: false,
-            url: false,
-          },
-          decisionType: DecisionType.DEFAULT,
-        },
-        options: [],
-        participants: [],
-        consigneeList: [],
-      }));
-      else return this.urlService.voteURL$.pipe(
-        switchMap(url => this.http.get<VoteModel<true>>(url + '/votes/edit/' + action.id, { withCredentials: true })),
-        map(deserializeVoteModel),
-        map(request => new InitVoteSuccessAction(request)),
-        catchError(err => of(new InitVoteErrorAction(err)))
-      );
-    })
-  ); });
-
-  postVote = createEffect(() => { return this.actions$.pipe(
-    ofType(VoteActions.POST),
-    switchMap(() => this.store.select(voteFeature.selectVoteState).pipe(
+  postVote = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(VoteActions.POST),
+      switchMap(() => this.store.select(voteFeature.selectVoteState).pipe(
       filter(state => state.complete),
       take(1),
     )),
-    map(state => ({ request: state.vote!, appointmentsEdited: state.appointmentsChanged })),
-    delayWhen(() => this.store.select(authFeature.selectAuthState).pipe(
-      filter(authState => !authState.busy),
-      take(1)
-    )),
-    concatLatestFrom(() => this.urlService.voteURL$),
-    map(([request, url]) => {
-      if (request.request.id) {
-        const sanatizedRequest: Partial<VoteModel> = { ...request.request };
-        if(!request.appointmentsEdited) {
-          delete sanatizedRequest.options;
-          delete sanatizedRequest.participants;
+      map(state => ({ request: state.vote!, appointmentsEdited: state.appointmentsChanged })),
+      delayWhen(() => this.store.select(authFeature.selectAuthState).pipe(
+        filter(authState => !authState.busy),
+        take(1),
+      )),
+      concatLatestFrom(() => this.urlService.voteURL$),
+      map(([request, url]) => {
+        if(request.request.id) {
+          const sanatizedRequest: Partial<VoteModel> = { ...request.request };
+          if(!request.appointmentsEdited) {
+            delete sanatizedRequest.options;
+            delete sanatizedRequest.participants;
+          }
+          return {
+            editToken: request.request.editToken || request.request.id?.toString(),
+            request: this.http.put<VoteModel<true>>(url + 'votes/edit/' + (request.request.editToken || request.request.id), sanatizedRequest, { withCredentials: true }),
+          };
         }
-        return { editToken: request.request.editToken || request.request.id?.toString(), request: this.http.put<VoteModel<true>>(url + 'votes/edit/' + (request.request.editToken || request.request.id), sanatizedRequest, { withCredentials: true }) };
-      }
-      return { request: this.http.post<VoteModel<true>>(url + 'votes', request.request, { withCredentials: true }) };
-    }),
-    switchMap(({ request, editToken }) => request.pipe(
-      map(deserializeVoteModel),
-      map(created => new PostVoteSuccessAction(created, editToken)),
-      catchError(err => of(new PostVoteErrorAction(err)))
-    ))
-  ); });
+        return { request: this.http.post<VoteModel<true>>(url + 'votes', request.request, { withCredentials: true }) };
+      }),
+      switchMap(({ request, editToken }) => request.pipe(
+        map(deserializeVoteModel),
+        map(created => new PostVoteSuccessAction(created, editToken)),
+        catchError(err => of(new PostVoteErrorAction(err))),
+      )),
+    );
+  });
 
-  successFullPost = createEffect(() => { return this.actions$.pipe(
-    ofType(VoteActions.POST_SUCCESS),
-    map((action: PostVoteSuccessAction) => this.router.navigate(['/vote/links'], {
-      queryParams: {
-        participationToken: action.created.participationToken || action.created.id,
-        editToken: action.created.editToken || action.created.id,
-      },
-    })),
-    switchMap(from)
-  ); }, { dispatch: false });
+  successFullPost = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(VoteActions.POST_SUCCESS),
+      map((action: PostVoteSuccessAction) => this.router.navigate(['/vote/links'], {
+        queryParams: {
+          participationToken: action.created.participationToken || action.created.id,
+          editToken: action.created.editToken || action.created.id,
+        },
+      })),
+      switchMap(from),
+    );
+  }, { dispatch: false });
 }
