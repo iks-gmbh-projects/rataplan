3f455626a07d5b36ba486b4fe4625577447adcc5
HartlIKS <T.Hartl@iks-gmbh.com>
2023-07-04 11:39:38 +0200

    Made Login page return to previous page even if accessed via login button

--- rataplan-frontend/src/app/services/auth-guard-service/profile-password-auth-service.ts
+++ rataplan-frontend/src/app/services/auth-guard-service/profile-password-auth-service.ts
@@ -13,15 +13,19 @@ export class ProfilePasswordAuthService implements CanActivate{
   ) {
   }
 
+  static createUrlTree(router: Router, state: RouterStateSnapshot = router.routerState.snapshot): UrlTree {
+    return router.createUrlTree(["/login"], {
+      queryParams: {
+        redirect: state.url,
+      },
+    })
+  }
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.store.select(authFeature.selectAuthState).pipe(
       filter(auth => !auth.busy),
       map(auth => !!auth.user),
-      map(allowed => allowed || this.router.createUrlTree(["/login"], {
-        queryParams: {
-          redirect: state.url,
-        },
-      }))
+      map(allowed => allowed || ProfilePasswordAuthService.createUrlTree(this.router, state))
     );
   }
 
