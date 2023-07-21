cca65ed3ffdfeb10b5f55f9e2572d49983f4ca62
jho <j.howard@iks-gmbh.com>
2023-07-21 16:22:40 +0200

    Test gegen unsignierte Tokens hinzugefügt

--- rataplan-backend/src/test/java/de/iks/rataplan/restservice/AuthServiceTest.java
+++ rataplan-backend/src/test/java/de/iks/rataplan/restservice/AuthServiceTest.java
@@ -66,4 +66,19 @@ public class AuthServiceTest {
 	public void getUserDataShouldFailInvalidToken() {
 		authService.getUserData("blashgiodrfngslkshdr");
 	}
+
+	@Test(expected = InvalidTokenException.class)
+	public void getUserDataShouldFailUnsignedToken() {
+		Claims claims = Jwts.claims();
+		claims.setIssuer("test");
+		claims.setSubject(AUTHUSER_1.getUsername());
+		claims.put(AuthServiceImpl.CLAIM_USERID, AUTHUSER_1.getId());
+		claims.put(AuthServiceImpl.CLAIM_PURPOSE, AuthServiceImpl.PURPOSE_LOGIN);
+		claims.setExpiration(new Date(System.currentTimeMillis()+1000));
+		keyExchangeConfig.setValidIssuer("test");
+
+		String token = Jwts.builder().setClaims(claims).compact();
+
+		authService.getUserData(token);
+	}
 }
