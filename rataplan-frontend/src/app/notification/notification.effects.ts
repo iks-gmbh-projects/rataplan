017e27e098c028df5ac7b8e05f1efbd1c37816c9
HartlIKS <T.Hartl@iks-gmbh.com>
2023-08-24 12:06:25 +0200

    Fix notification fetch spam

--- rataplan-frontend/src/app/notification/notification.effects.ts
+++ rataplan-frontend/src/app/notification/notification.effects.ts
@@ -1,17 +1,18 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FrontendUser } from '../models/user.model';
 import { notificationActions } from './notification.actions';
-import { filter, map, tap } from 'rxjs/operators';
+import { distinctUntilChanged, distinctUntilKeyChanged, filter, map, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { authFeature } from '../authentication/auth.feature';
 import { of, sample, startWith, switchMap } from 'rxjs';
-import { AuthActions } from '../authentication/auth.actions';
+import { AuthActions, LoginSuccessAction } from '../authentication/auth.actions';
 import { voteNotificationtypes } from '../vote/vote.notificationtypes';
 import { Store } from '@ngrx/store';
 import { VoteListService } from '../services/dashboard-service/vote-list.service';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class NotificationEffects {
   constructor(
@@ -19,31 +20,29 @@ export class NotificationEffects {
     private store: Store,
     private snackBars: MatSnackBar,
     private voteListService: VoteListService,
-  ) {
+  )
+  {
   }
-
+  
   notifyEffect = createEffect(() => {
     return this.$actions.pipe(
       ofType(notificationActions.notify),
       filter(notif => !!notif.message),
-      tap(notif => this.snackBars.open(notif.message!, "OK")),
+      tap(notif => this.snackBars.open(notif.message!, 'OK')),
     );
-  }, { dispatch: false });
-
+  }, {dispatch: false});
+  
   clearOnLogout = createEffect(() => {
     return this.$actions.pipe(
       ofType(AuthActions.LOGOUT_ACTION),
       map(() => notificationActions.clearall()),
     );
   });
-
+  
   loadVoteNotifications = createEffect(() => {
-    return this.store.select(authFeature.selectUser).pipe(
-      sample(this.$actions.pipe(
-        ofType(AuthActions.LOGIN_SUCCESS_ACTION),
-        startWith(null),
-      )),
-      filter(u => !!u),
+    return this.$actions.pipe(
+      ofType(AuthActions.LOGIN_SUCCESS_ACTION),
+      map((a: LoginSuccessAction) => a.payload),
       switchMap(() => this.voteListService.getCondignedVotes()),
       map(v => v.length),
       switchMap(n => of(
