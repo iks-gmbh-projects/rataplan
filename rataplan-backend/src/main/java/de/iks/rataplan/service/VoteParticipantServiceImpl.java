6b897c39a8b8cf36680c84535d1066ace7576932
jho <j.howard@iks-gmbh.com>
2023-08-24 13:43:15 +0200

    Backend validation of participant decisions during update and creation

--- rataplan-backend/src/main/java/de/iks/rataplan/service/VoteParticipantServiceImpl.java
+++ rataplan-backend/src/main/java/de/iks/rataplan/service/VoteParticipantServiceImpl.java
@@ -1,19 +1,21 @@
 package de.iks.rataplan.service;
 
-import de.iks.rataplan.domain.Decision;
-import de.iks.rataplan.domain.Vote;
-import de.iks.rataplan.domain.VoteDecision;
-import de.iks.rataplan.domain.VoteParticipant;
+import de.iks.rataplan.domain.*;
 import de.iks.rataplan.exceptions.ForbiddenException;
 import de.iks.rataplan.exceptions.MalformedException;
 import de.iks.rataplan.repository.VoteParticipantRepository;
 import de.iks.rataplan.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Transactional
@@ -28,14 +30,15 @@ public class VoteParticipantServiceImpl implements VoteParticipantService {
     public VoteParticipant createParticipant(Vote vote, VoteParticipant voteParticipant) {
         
         this.validateExpirationDate(vote);
-        if (!this.validateDecisions(vote,voteParticipant))throw new MalformedException("bad");
+        if(!this.validateDecisions(vote, voteParticipant))
+            throw new MalformedException("Vote decision violate yes limit or participant limit configuration");
         voteParticipant.setId(null);
         
-        if (vote.validateDecisionsForParticipant(voteParticipant)) {
+        if(vote.validateDecisionsForParticipant(voteParticipant)) {
             voteParticipant.setVote(vote);
             vote.getParticipants().add(voteParticipant);
             
-            for (VoteDecision decision : voteParticipant.getVoteDecisions()) {
+            for(VoteDecision decision : voteParticipant.getVoteDecisions()) {
                 decision.setVoteParticipant(voteParticipant);
             }
             
@@ -56,72 +59,78 @@ public class VoteParticipantServiceImpl implements VoteParticipantService {
     
     @Override
     public VoteParticipant updateParticipant(
-        Vote vote, VoteParticipant dbVoteParticipant,
-        VoteParticipant newVoteParticipant
-    ) {
+        Vote vote, VoteParticipant dbVoteParticipant, VoteParticipant newVoteParticipant
+    )
+    {
         
         this.validateExpirationDate(vote);
         
-        if (!vote.validateDecisionsForParticipant(newVoteParticipant)) {
+        if(!vote.validateDecisionsForParticipant(newVoteParticipant)) {
             throw new MalformedException("VoteDecisions don't fit the DecisionType in the Vote.");
         }
         
         dbVoteParticipant.setName(newVoteParticipant.getName());
         dbVoteParticipant.setVote(vote);
-
-        if (!this.validateDecisions(vote,newVoteParticipant)) throw new MalformedException("bad");
-
+        
+        if(!this.validateDecisions(vote, newVoteParticipant))
+            throw new MalformedException("Vote decision violate yes limit or participant limit configuration");
+        
         this.updateDecisionsForParticipant(dbVoteParticipant.getVoteDecisions(), newVoteParticipant.getVoteDecisions());
         return voteParticipantRepository.saveAndFlush(dbVoteParticipant);
     }
-
-    public boolean validateDecisions(Vote vote, VoteParticipant voteParticipant){
-        if (vote.getVoteConfig().getYesLimitActive()) {
-            if (voteParticipant.getVoteDecisions().isEmpty()) return false;
-            else {
-                long yesVotes = voteParticipant.getVoteDecisions()
-                        .stream()
-                        .filter(decision -> decision.getDecision() == Decision.ACCEPT)
-                        .count();
-                return yesVotes <= vote.getVoteConfig().getYesAnswerLimit();
-            }
+    
+    public boolean validateDecisions(Vote vote, VoteParticipant voteParticipant) {
+        if(voteParticipant.getVoteDecisions().isEmpty()) return false;
+        if(vote.getVoteConfig().getYesLimitActive()) {
+            long yesVotes = voteParticipant.getVoteDecisions()
+                .stream()
+                .filter(decision -> decision.getDecision() == Decision.ACCEPT)
+                .count();
+            if(yesVotes == vote.getVoteConfig().getYesAnswerLimit()) return false;
         }
-        return true;
+        
+        return Stream.concat(
+                vote.getParticipants().stream().filter(p -> !Objects.equals(p.getId(), voteParticipant.getId())),
+                Stream.of(voteParticipant)
+            )
+            .map(VoteParticipant::getVoteDecisions)
+            .flatMap(Collection::stream)
+            .filter(d -> d.getDecision() == Decision.ACCEPT)
+            .collect(Collectors.groupingBy(VoteDecision::getVoteOption, Collectors.counting()))
+            .entrySet()
+            .stream()
+            .filter(e -> e.getKey().isParticipantLimitActive())
+            .allMatch(e -> e.getKey().getParticipantLimit() >= e.getValue());
     }
-
+    
     @Override
-    public void anonymizeParticipants(int userId) {
-        voteParticipantRepository.findByUserId(userId)
-            .peek(member -> {
-                member.setName(null);
-                member.setUserId(null);
-            })
-            .forEach(voteParticipantRepository::save);
+    public void anonymizeParticipants(int id) {
+        VoteParticipant member = voteParticipantRepository.findOne(id);
+        member.setName(null);
+        member.setUserId(null);
+        voteParticipantRepository.saveAndFlush(member);
     }
-
+    
     /**
      * updates the oldDecisions decisions to the new ones based on the
      * voteOptionId's
-     *
-     * @param oldDecisions
-     * @param newDecisions
      */
     private void updateDecisionsForParticipant(
-        List<VoteDecision> oldDecisions,
-        List<VoteDecision> newDecisions
-    ) {
-        for (VoteDecision voteDecision : oldDecisions) {
-            for (VoteDecision newdecision : newDecisions) {
-                if (Objects.equals(voteDecision.getVoteOption().getId(), newdecision.getVoteOption().getId())) {
+        List<VoteDecision> oldDecisions, List<VoteDecision> newDecisions
+    )
+    {
+        for(VoteDecision voteDecision : oldDecisions) {
+            for(VoteDecision newdecision : newDecisions) {
+                if(Objects.equals(voteDecision.getVoteOption().getId(), newdecision.getVoteOption().getId())) {
                     voteDecision.setDecision(newdecision.getDecision());
                     voteDecision.setParticipants(newdecision.getParticipants());
                 }
             }
         }
     }
-
+    
     private void validateExpirationDate(Vote vote) {
-        if (vote.isNotified()) {
+        if(vote.isNotified()) {
             throw new ForbiddenException("Vote is expired!");
         }
     }
