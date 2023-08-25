d3d4699b46e7e3cfe749ae26aedac453048fcb77
HartlIKS <T.Hartl@iks-gmbh.com>
2023-08-25 12:43:03 +0200

    Fix missing @Injectable warning on EUDateAdapter

--- rataplan-frontend/src/app/eu-date-adapter.ts
+++ rataplan-frontend/src/app/eu-date-adapter.ts
@@ -1,14 +1,16 @@
+import { Injectable } from '@angular/core';
 import { NativeDateAdapter } from '@angular/material/core';
 
-
+@Injectable()
 export class EUDateAdapter extends NativeDateAdapter {
-
-
+  
   override parse(date: string): Date | null {
     const match = date.match(/^(\d{1,2})[-/\\.](\d{1,2})[-/\\.](\d+)$/);
-    if (match) {
-      const year = (match[3].length > 2 ? 0 : 2000) + Number(match[3]);
-      const month = Number(match[2])-1;
+    if(match) {
+      const year = (
+        match[3].length > 2 ? 0 : 2000
+      ) + Number(match[3]);
+      const month = Number(match[2]) - 1;
       const day = Number(match[1]);
       const d = new Date(year, month, day);
       if(d.getFullYear() != year || d.getMonth() != month || d.getDate() != day) {
@@ -18,6 +20,5 @@ export class EUDateAdapter extends NativeDateAdapter {
     }
     return null;
   }
-
-
+  
 }
