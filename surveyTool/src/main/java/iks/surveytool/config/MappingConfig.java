826cdabd838ebc69fd1fdcd184ff9d95c4386bd7
HartlIKS <T.Hartl@iks-gmbh.com>
2023-09-13 17:31:56 +0200

    Update Spring to version 2.7.15

--- surveyTool/src/main/java/iks/surveytool/config/MappingConfig.java
+++ surveyTool/src/main/java/iks/surveytool/config/MappingConfig.java
@@ -1,6 +1,6 @@
 package iks.surveytool.config;
 
-import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +11,7 @@ import java.util.List;
 public class MappingConfig {
 
     @Bean
-    public ModelMapper modelMapper(List<AbstractConverter<?,?>> converters) {
+    public ModelMapper modelMapper(List<Converter<?,?>> converters) {
         ModelMapper modelMapper = new ModelMapper();
         converters.forEach(modelMapper::addConverter);
         return modelMapper;
