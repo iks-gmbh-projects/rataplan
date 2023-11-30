ec6b724edcc80c2f04bf9f3d3071f974943c2655
jho <j.howard@iks-gmbh.com>
2023-11-30 11:06:21 +0100

    Benachrichtung nach Rückmeldungabgabe umgesetzt

--- rataplan-frontend/src/app/feedback/feedback.component.ts
+++ rataplan-frontend/src/app/feedback/feedback.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { catchError, of } from 'rxjs';
+
 import { FormErrorMessageService } from '../services/form-error-message-service/form-error-message.service';
 import { FeedbackCategory, FeedbackService } from './feedback.service';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+  styleUrls: ['./feedback.component.css'],
 })
 export class FeedbackComponent {
   readonly form = new FormGroup({
@@ -18,10 +21,11 @@ export class FeedbackComponent {
   
   readonly categories = FeedbackCategory;
   readonly ratings = [1, 2, 3, 4, 5] as const;
-
+  
   constructor(
     private readonly feedbackService: FeedbackService,
-    readonly errorMessageService: FormErrorMessageService
+    readonly errorMessageService: FormErrorMessageService,
+    private matSnackBar: MatSnackBar,
   )
   {
   }
@@ -31,15 +35,22 @@ export class FeedbackComponent {
     if(ratingControl.value === rating) ratingControl.setValue(0);
     else ratingControl.setValue(rating);
   }
-
+  
   submit() {
     if(!this.form.valid) return;
     const val = this.form.value;
+    const config = new MatSnackBarConfig();
+    config.duration = 10000;
     this.feedbackService.submitFeedback({
       title: val.title!,
       text: val.text!,
       rating: val.rating!,
       category: val.category!,
-    }).subscribe(() => {});
+    }).pipe(catchError(() => of(false)))
+      .subscribe((next) => {
+          if(next) this.matSnackBar.open('Abgabe erflogreich', '', config);
+          else this.matSnackBar.open('Abgabe fehlgeschlagen', '', config);
+        },
+      );
   }
 }
