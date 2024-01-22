8cfb6202a2351b3430e65d74f1e77822f6d0a99d
HartlIKS <T.Hartl@iks-gmbh.com>
2024-01-22 15:30:26 +0100

    split group update into renaming and adding/removing

--- rataplan-auth/src/main/java/de/iks/rataplan/service/ContactService.java
+++ rataplan-auth/src/main/java/de/iks/rataplan/service/ContactService.java
@@ -9,6 +9,8 @@ public interface ContactService {
     AllContactsDTO getContacts(User currentUser);
     void deleteContact(User currentUser, int toDelete);
     ContactGroupDTO createGroup(User currentUser, ContactGroupDTO group);
-    ContactGroupDTO updateGroup(User currentUser, ContactGroupDTO group);
+    ContactGroupDTO renameGroup(User currentUser, long groupId, String newName);
+    ContactGroupDTO addToGroup(User currentUser, long groupId, int contact);
+    ContactGroupDTO removeFromGroup(User currentUser, long groupId, int contactId);
     void deleteGroup(User currentUser, long idToDelete);
 }
