457e346c1ad01831ee71394e6874d527911b55ff
HartlIKS <T.Hartl@iks-gmbh.com>
2024-02-01 12:01:16 +0100

    Use move to spring's natively supported version of flyway, remove flyway config and separate integration test resources from test resources.

--- rataplan-backend/src/integration/java/de/iks/rataplan/config/IntegrationConfig.java
+++ rataplan-backend/src/integration/java/de/iks/rataplan/config/IntegrationConfig.java
@@ -11,15 +11,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.util.Properties;
-import java.util.TimeZone;
 
 @Profile("integration")
 @PropertySource("classpath:integration.properties")
@@ -27,11 +18,11 @@ import java.util.TimeZone;
 @TestConfiguration
 public class IntegrationConfig {
     private final MockCryptoService mockCryptoService = new MockCryptoService();
-    private final FromEncryptedStringConverter fromEncryptedStringConverter = new FromEncryptedStringConverter(mockCryptoService);
-    private final ToEncryptedStringConverter toEncryptedStringConverter = new ToEncryptedStringConverter(mockCryptoService);
-
-	private final Environment environment;
-
+    private final FromEncryptedStringConverter fromEncryptedStringConverter = new FromEncryptedStringConverter(
+        mockCryptoService);
+    private final ToEncryptedStringConverter toEncryptedStringConverter = new ToEncryptedStringConverter(
+        mockCryptoService);
+    
     @Bean
     public ModelMapper modelMapper(DecisionConverter decisionConverter) {
         ModelMapper mapper = new ModelMapper();
@@ -41,26 +32,4 @@ public class IntegrationConfig {
         mapper.addConverter(fromEncryptedStringConverter);
         return mapper;
     }
-
-    @Bean
-    public DataSource dataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder
-                .setType(EmbeddedDatabaseType.H2)
-                .build();
-        return db;
-    }
-    
-    @Bean 
-    public Properties additionalHibernateProperties() {
-    	Properties additionalProperties = new Properties();
-        additionalProperties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        additionalProperties.put("hibernate.show_sql", "true");
-        additionalProperties.put("hibernate.hbm2ddl.auto", "validate");
-        return additionalProperties;
-    }
-	@PostConstruct
-	void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("GMT+2"));
-	}
 }
