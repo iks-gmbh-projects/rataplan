0265b241c5a07ee9de55d3053e4ba3a9f74da311
HartlIKS <T.Hartl@iks-gmbh.com>
2024-02-15 08:07:32 +0100

    Enable Transactions

--- rataplan-auth/src/main/java/de/iks/rataplan/RataplanAuthMain.java
+++ rataplan-auth/src/main/java/de/iks/rataplan/RataplanAuthMain.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.core.SpringVersion;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableScheduling
 @Slf4j
 @EnableWebMvc
+@EnableTransactionManagement
 public class RataplanAuthMain
 {
     public static void main(String[] args) {
