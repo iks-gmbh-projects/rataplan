545852e532a0bc34f48ea49dcfc37cdfcad467f0
HartlIKS <T.Hartl@iks-gmbh.com>
2024-03-18 12:59:09 +0100

    add schedule to send digests.

--- rataplan-auth/src/main/java/de/iks/rataplan/service/SchedulerService.java
+++ rataplan-auth/src/main/java/de/iks/rataplan/service/SchedulerService.java
@@ -1,5 +1,6 @@
 package de.iks.rataplan.service;
 
+import de.iks.rataplan.domain.notifications.EmailCycle;
 import de.iks.rataplan.repository.AuthTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,12 +9,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
-
+import java.util.Set;
 
 @Transactional
 @Component
 @RequiredArgsConstructor
 public class SchedulerService {
+    private final NotificationService notificationService;
     private final AuthTokenRepository authTokenRepository;
 
     @Value("${token.lifetime}")
@@ -27,5 +29,15 @@ public class SchedulerService {
         currentDate.setTime(currentDate.getTime() - tokenLifetime);
 
         authTokenRepository.deleteAllByCreatedDateTimeIsBefore(currentDate);
+    }
+    
+    @Scheduled(cron = "0 2 0 1-5 * *")
+    public void sendDailyDigests() {
+        notificationService.sendSummary(Set.of(EmailCycle.DAILY_DIGEST));
+    }
+    
+    @Scheduled(cron = "0 2 0 6 * *")
+    public void sendWeeklyDigests() {
+        notificationService.sendSummary(Set.of(EmailCycle.DAILY_DIGEST, EmailCycle.WEEKLY_DIGEST));
     }
 }
