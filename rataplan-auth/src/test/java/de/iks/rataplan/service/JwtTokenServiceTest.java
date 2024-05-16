2ab92b20791d80a20fd3fbb9223c999fdc68bf40
jho <j.howard@iks-gmbh.com>
2024-05-16 15:34:55 +0200

    auth token tests löschen und jwt jwt test hinzufügen

--- rataplan-auth/src/test/java/de/iks/rataplan/service/JwtTokenServiceTest.java
+++ rataplan-auth/src/test/java/de/iks/rataplan/service/JwtTokenServiceTest.java
@@ -24,13 +24,11 @@ public class JwtTokenServiceTest {
     private io.jsonwebtoken.SigningKeyResolver signingKeyResolver;
     @Mock
     private CryptoService cryptoService;
-	private JwtTokenService jwtTokenService;
+    private JwtTokenService jwtTokenService;
     
     @BeforeEach
     void setup() throws NoSuchAlgorithmException {
-        jwtTokenService = new JwtTokenServiceImpl(
-            cryptoService, new JwtConfig(), signingKeyResolver
-        );
+        jwtTokenService = new JwtTokenServiceImpl(cryptoService, new JwtConfig(), signingKeyResolver);
         KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
         gen.initialize(2048);
         KeyPair idKey = gen.generateKeyPair();
@@ -39,37 +37,44 @@ public class JwtTokenServiceTest {
         lenient().when(cryptoService.idKeyP()).thenReturn(idKey.getPrivate());
         lenient().when(signingKeyResolver.resolveSigningKey(Mockito.any(), Mockito.any(Claims.class)))
             .thenReturn(signingKey.getPublic());
-        
     }
-
-	@Test
-	void generateTokenAndValidateTokenAndGetUsernameFromToken() {
-		UserDTO user = new UserDTO();
-
-		user.setUsername("Peter");
-		user.setMail("peter@sch.mitz");
-//		user.setPassword("geheim");
-
-		String token = jwtTokenService.generateLoginToken(user);
-		assertNotNull(token);
-
-		assertTrue(jwtTokenService.isTokenValid(token));
-
-		String username = jwtTokenService.getUsernameFromToken(token);
-		assertEquals(username, "Peter");
-	}
-
-	@Test
-	void generateAccountConfirmationAndRetrieveId(){
-		UserDTO user = new UserDTO();
-		user.setId(1);
-
-		String token = jwtTokenService.generateAccountConfirmationToken(user);
-		Integer userId = jwtTokenService.getUserIdFromAccountConfirmationToken(token);
-
-		assertNotNull(token);
-		assertTrue(jwtTokenService.isTokenValid(token));
-		assertEquals(userId,user.getId());
-	}
-
+    
+    @Test
+    void generateTokenAndValidateTokenAndGetUsernameFromToken() {
+        UserDTO user = new UserDTO();
+        
+        user.setUsername("Peter");
+        user.setMail("peter@sch.mitz");
+        //		user.setPassword("geheim");
+        
+        String token = jwtTokenService.generateLoginToken(user);
+        assertNotNull(token);
+        
+        assertTrue(jwtTokenService.isTokenValid(token));
+        
+        String username = jwtTokenService.getUsernameFromToken(token);
+        assertEquals(username, "Peter");
+    }
+    
+    @Test
+    void generateAccountConfirmationAndRetrieveId() {
+        UserDTO user = new UserDTO();
+        user.setId(1);
+        
+        String token = jwtTokenService.generateAccountConfirmationToken(user);
+        Integer userId = jwtTokenService.getUserIdFromAccountConfirmationToken(token);
+        
+        assertNotNull(token);
+        assertTrue(jwtTokenService.isTokenValid(token));
+        assertEquals(userId, user.getId());
+    }
+    
+    @Test
+    void generateResetPasswordTokenAndRetrieveEmail() {
+        String email = "test@test.com";
+        String jwt = this.jwtTokenService.generateResetPasswordToken(email);
+        assertNotNull(jwt);
+        assertTrue(jwtTokenService.isTokenValid(jwt));
+        assertEquals(email, this.jwtTokenService.getEmailFromResetPasswordToken(jwt));
+    }
 }
