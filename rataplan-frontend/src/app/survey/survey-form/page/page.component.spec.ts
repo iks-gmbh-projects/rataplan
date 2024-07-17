5a3655409f30d5008776ef3edf521f450faec633
HartlIKS <T.Hartl@iks-gmbh.com>
2024-07-17 08:41:11 +0200

    migrate forms to non-legacy material components

--- rataplan-frontend/src/app/survey/survey-form/page/page.component.spec.ts
+++ rataplan-frontend/src/app/survey/survey-form/page/page.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
 
 import { PageComponent } from './page.component';
