1301204a599391bdd1ffcbd0491d76716222204d
jho <j.howard@iks-gmbh.com>
2024-07-19 12:30:02 +0200

    db migration

---
+++ rataplan-backend/src/main/resources/migration/prod/V25__ADD_IS_END_OR_START_TIME.sql
@@ -0,0 +1,2 @@
+ALTER TABLE vote ADD COLUMN isstarttime boolean;
+ALTER TABLE vote ADD COLUMN isendtime boolean;
---
+++ rataplan-backend/src/main/resources/migration/prod/V26__DELETE_YES_AND_PARTICIPATION_LIMIT_ACTIVE.sql
@@ -0,0 +1,2 @@
+alter table vote drop column yesLimitActive;
+alter table public.voteoption drop column participantlimitactive;
