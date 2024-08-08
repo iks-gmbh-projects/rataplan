1ce9b5464a066c07783e85eb671d34e6b1864db2
HartlIKS <T.Hartl@iks-gmbh.com>
2024-08-08 11:52:57 +0200

    fix move auth

--- rataplan-frontend/src/app/validate-profile-update/validate-profile-update.component.ts
+++ rataplan-frontend/src/app/validate-profile-update/validate-profile-update.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { ChangeProfileDetailsAction } from '../authentication/auth.actions';
+import { authActions } from '../authentication/auth.actions';
 import { FrontendUser } from '../models/user.model';
 import { FormErrorMessageService } from '../services/form-error-message-service/form-error-message.service';
 
@@ -12,24 +12,27 @@ import { FormErrorMessageService } from '../services/form-error-message-service/
   styleUrls: ['./validate-profile-update.component.css'],
 })
 export class ValidateProfileUpdateComponent implements OnInit {
-  
+
   enterPassword = false;
   password = new FormControl<string | undefined>(undefined, Validators.required);
-  
+
   constructor(
     public errorMessagingService: FormErrorMessageService,
     @Inject(MAT_DIALOG_DATA) public data: FrontendUser,
     private store: Store,
-  )
-  { }
-  
+  ) {
+  }
+
   ngOnInit(): void {
   }
-  
+
   confirmProfileChanges() {
-    this.store.dispatch(new ChangeProfileDetailsAction(
-      {...this.data, password: this.password.value!},
-    ));
+    this.store.dispatch(authActions.changeProfileDetails({
+      user: {
+        ...this.data,
+        password: this.password.value!,
+      },
+    }));
   }
-  
+
 }
