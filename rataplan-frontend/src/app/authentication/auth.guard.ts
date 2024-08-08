47b649e4dce6bf509433f7004550b4a8d663f6b0
HartlIKS <T.Hartl@iks-gmbh.com>
2024-08-08 12:10:06 +0200

    make auth guard actually store redirect url

--- rataplan-frontend/src/app/authentication/auth.guard.ts
+++ rataplan-frontend/src/app/authentication/auth.guard.ts
@@ -1,17 +1,27 @@
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { debounceTime, first } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { authFeature } from './auth.feature';
 
-export function ensureLoggedIn() {
+export function getURL(route: ActivatedRouteSnapshot): string {
+  return route.pathFromRoot.flatMap(v => v.url).map(s => s.toString()).join('/') +
+  (route.queryParams ? `?${Object.entries(route.queryParams).map(([k, v]) => `${k}=${v}`).join('&')}` : '') +
+  (route.fragment ? `#${route.fragment}` : '')
+}
+
+export function ensureLoggedIn(next: ActivatedRouteSnapshot) {
   const router = inject(Router);
   return inject(Store).select(authFeature.selectAuthState).pipe(
     debounceTime(1),
     first(auth => !auth.busy),
     map(({user}) => user !== undefined),
-    map(allowed => allowed || router.createUrlTree(["/"]))
+    map(allowed => allowed || router.createUrlTree(["/login"], {
+      queryParams: {
+        redirect: getURL(next),
+      },
+    })),
   );
 }
 
