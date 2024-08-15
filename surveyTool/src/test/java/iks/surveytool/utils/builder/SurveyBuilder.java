feea6e7c6f11c59ff5c08dc62dcebe20a44d68a8
jho <j.howard@iks-gmbh.com>
2024-08-15 11:28:27 +0200

    tests und db migration aktualisieren

--- surveyTool/src/test/java/iks/surveytool/utils/builder/SurveyBuilder.java
+++ surveyTool/src/test/java/iks/surveytool/utils/builder/SurveyBuilder.java
@@ -3,19 +3,19 @@ package iks.surveytool.utils.builder;
 import iks.surveytool.entities.Survey;
 
 import java.nio.charset.StandardCharsets;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SurveyBuilder {
-
+    
     private static final byte[] description = "Default Beschreibung".getBytes(StandardCharsets.UTF_8);
-    private static final ZonedDateTime startDate = ZonedDateTime.of(2050, 1, 1, 12, 0, 0, 0, ZoneId.systemDefault());
+    private static final ZonedDateTime startDate = ZonedDateTime.of(LocalDateTime.now(), ZoneId.systemDefault());
     private static final ZonedDateTime endDate = startDate.plusWeeks(1L);
     private static final boolean openAccess = false;
     private static final boolean anonymousParticipation = false;
     private static final String accessId = "Test ID";
-
+    
     public static Survey createDefaultSurvey() {
         Survey newSurvey = new Survey();
         newSurvey.setId(1L);
@@ -24,19 +24,22 @@ public class SurveyBuilder {
         setDefaults(newSurvey);
         return newSurvey;
     }
-
-    public static Survey createSurveyWithDefaultDate(Long id,
-                                              String name) {
+    
+    public static Survey createSurveyWithDefaultDate(
+        Long id, String name
+    )
+    {
         Survey newSurvey = new Survey();
         newSurvey.setId(id);
         newSurvey.setName(name == null ? null : name.getBytes(StandardCharsets.UTF_8));
         setDefaults(newSurvey);
         return newSurvey;
     }
-
-    public static Survey createSurveyWithUserAndDefaultDate(Long id,
-                                                     String name,
-                                                     Long userId) {
+    
+    public static Survey createSurveyWithUserAndDefaultDate(
+        Long id, String name, Long userId
+    )
+    {
         Survey newSurvey = new Survey();
         newSurvey.setId(id);
         newSurvey.setName(name == null ? null : name.getBytes(StandardCharsets.UTF_8));
@@ -44,7 +47,7 @@ public class SurveyBuilder {
         setDefaults(newSurvey);
         return newSurvey;
     }
-
+    
     private static void setDefaults(Survey newSurvey) {
         newSurvey.setDescription(description);
         newSurvey.setStartDate(startDate);
