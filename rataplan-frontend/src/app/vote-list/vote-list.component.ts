453edd3dfa6aa0fe06a3d0b1942e0359e90de3b9
jho <j.howard@iks-gmbh.com>
2024-09-24 12:08:34 +0200

    fix rebase errors

--- rataplan-frontend/src/app/vote-list/vote-list.component.ts
+++ rataplan-frontend/src/app/vote-list/vote-list.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { delay, Observable, of, switchMap } from 'rxjs';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Actions, ofType } from '@ngrx/effects';
-import { BehaviorSubject, combineLatestAll, delay, Observable, of, Subject, Subscription, switchMap, takeUntil } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { delay, Observable, of, Subscription, switchMap } from 'rxjs';
 
 import { VoteModel } from '../models/vote.model';
 import { notificationActions } from '../notification/notification.actions';
-import { VoteListService } from '../services/vote-list-service/vote-list.service';
-import { VoteService } from '../vote/vote/vote-service/vote.service';
+import { Notification } from '../websocket-service/websocket-service';
+import { voteListAction } from './state/vote-list.action';
 import { voteListFeature } from './state/vote-list.feature';
 
 @Component({
@@ -16,16 +14,18 @@ import { voteListFeature } from './state/vote-list.feature';
   templateUrl: './vote-list.component.html',
   styleUrls: ['./vote-list.component.css'],
 })
-export class VoteListComponent implements OnInit {
+export class VoteListComponent {
   protected readonly busy$: Observable<boolean>;
   protected readonly delayedBusy$: Observable<boolean>;
   protected readonly created$: Observable<VoteModel[]>;
   protected readonly consigned$: Observable<VoteModel[]>;
   protected readonly consignedNonExpired$: Observable<number>;
   protected readonly participated$: Observable<VoteModel[]>;
+  private notificationSub$: Subscription;
   
   constructor(
     private readonly store: Store,
+    private readonly actions: Actions,
   )
   {
     this.busy$ = store.select(voteListFeature.selectBusy);
@@ -36,17 +36,10 @@ export class VoteListComponent implements OnInit {
     this.consigned$ = store.select(voteListFeature.selectConsigned);
     this.consignedNonExpired$ = store.select(voteListFeature.selectNonExpiredConsignedCount);
     this.participated$ = store.select(voteListFeature.selectParticipated);
-  }
-  
-  public ngOnInit(): void {
     this.notificationSub$ = this.actions
       .pipe(ofType(notificationActions.invitation))
-      .subscribe(v => {
-        this.vs.getVoteByParticipationToken(v.link)
-          .subscribe(r => {
-            this.consignedVotes.push(r);
-            this.consignedNonExpiredVoteCount++;
-          });
+      .subscribe((n: Notification) => {
+        store.dispatch(voteListAction.fetch());
       });
   }
   
--- rataplan-frontend/src/app/vote/vote-form/overview-subform/overview-subform.component.ts
+++ rataplan-frontend/src/app/vote/vote-form/overview-subform/overview-subform.component.ts
@@ -7,10 +7,7 @@ import { filter, Observable, of, Subscription } from 'rxjs';
 
 import { VoteOptionConfig, VoteOptionModel } from '../../../models/vote-option.model';
 import { FormErrorMessageService } from '../../../services/form-error-message-service/form-error-message.service';
-import { ConfigSubformComponent } from '../config-subform/config-subform.component';
 import { ExtraValidators } from '../../../validator/validators';
-import { AddVoteOptionsAction, EditVoteOptionAction, RemoveVoteOptionAction } from '../../vote.actions';
-import { voteFeature } from '../../vote.feature';
 import { ConfigSubformComponent } from '../config-subform/config-subform.component';
 import { ConfirmChoiceComponent } from '../confirm-choice/confirm-choice.component';
 import { CONFIRM_CHOICE_OPTIONS, VoteOptionDecisionType } from '../decision-type.enum';
