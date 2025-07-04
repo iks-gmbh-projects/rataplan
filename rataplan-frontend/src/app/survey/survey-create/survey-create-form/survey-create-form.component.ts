3e892ee1b00d479153924566e8e8f10a1122dbe1
Corinna Biermann <cbi@iks-gmbh.com>
2025-07-04 11:07:42 +0200

    added headline for surveys

--- rataplan-frontend/src/app/survey/survey-create/survey-create-form/survey-create-form.component.ts
+++ rataplan-frontend/src/app/survey/survey-create/survey-create-form/survey-create-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import {Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { FormErrorMessageService } from '../../../services/form-error-message-service/form-error-message.service';
 import { surveyCreateActions } from '../state/survey-create.action';
 import { surveyCreateFeature } from '../state/survey-create.feature';
@@ -15,6 +16,7 @@ import { SurveyCreateFormPageComponent } from './survey-create-form-page/survey-
 export class SurveyCreateFormComponent {
   public readonly headPage$: Observable<boolean>;
   public readonly valid$: Observable<boolean>;
+  editing: boolean = false;
   
   @ViewChild('headForm') headForm?: SurveyCreateFormHeadComponent;
   @ViewChild('pageForm') pageForm?: SurveyCreateFormPageComponent;
@@ -22,10 +24,18 @@ export class SurveyCreateFormComponent {
   constructor(
     private readonly store: Store,
     public readonly errorMessageService: FormErrorMessageService,
+    private route: ActivatedRoute,
   )
   {
     this.headPage$ = store.select(surveyCreateFeature.selectHeadPage);
     this.valid$ = store.select(surveyCreateFeature.selectAllValid);
+  }
+  
+  ngOnInit() {
+    const url = this.route.snapshot.url.map(segment => segment.path).join('/');
+    this.editing = (
+      url.includes('edit')
+    );
   }
   
   public submit(preview: boolean = false): void {
