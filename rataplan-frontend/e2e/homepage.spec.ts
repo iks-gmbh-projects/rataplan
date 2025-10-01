ed958bcb838ca8fe795b2c193f5b1f8c19c574e2
vpe <v.peters@iks-gmbh.com>
2025-10-01 14:48:30 +0200

    added test for 'change url on "neue Abstimmung"'

--- rataplan-frontend/e2e/homepage.spec.ts
+++ rataplan-frontend/e2e/homepage.spec.ts
@@ -5,3 +5,10 @@ test('has title', async({page}) => {
   
   await expect(page).toHaveTitle(/drumdibum/);
 });
+
+test('change url on "neue Abstimmung"', async({page}) => {
+  await page.goto('localhost:4200');
+  
+  await page.getByRole('link', {name: 'Neue Abstimmung'}).click();
+  await expect(page).toHaveURL(/vote\/create\/general/);
+});
