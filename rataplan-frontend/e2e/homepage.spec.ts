e3e9efe1a9fcbd820cd7e78ad6ce635ae35f6a43
vpe <v.peters@iks-gmbh.com>
2025-10-01 15:16:23 +0200

    outsourced await page.goto('localhost:4200'); in beforeEach

--- rataplan-frontend/e2e/homepage.spec.ts
+++ rataplan-frontend/e2e/homepage.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async({page}) => {
+test.beforeEach(async({page}) => {
   await page.goto('localhost:4200');
-  
+});
+
+test('has title', async({page}) => {
   await expect(page).toHaveTitle(/drumdibum/);
 });
 
 test('change url on "neue Abstimmung"', async({page}) => {
-  await page.goto('localhost:4200');
-  
   await page.getByRole('link', {name: 'Neue Abstimmung'}).click();
   await expect(page).toHaveURL(/vote\/create\/general/);
 });
