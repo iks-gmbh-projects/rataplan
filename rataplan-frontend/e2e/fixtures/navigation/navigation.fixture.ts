0eb0263c9d1d5b85b0500620f355f8aa548b0072
vpe <v.peters@iks-gmbh.com>
2025-10-02 14:49:31 +0200

    fill navigationTest with functions

--- rataplan-frontend/e2e/fixtures/navigation/navigation.fixture.ts
+++ rataplan-frontend/e2e/fixtures/navigation/navigation.fixture.ts
@@ -9,4 +9,23 @@ type NavigationFixture = {
   navigationToUmfragen: NavigationToUmfragen;
 }
 
-export const navigationTest = base.extend<NavigationFixture>({});
+export const navigationTest = base.extend<NavigationFixture>({
+  navigationToAbstimmung: async({page}, use) => {
+    await page.goto('localhost:4200');
+    await page.getByRole('button', {name: 'Toggle sidenav'}).click();
+    await page.getByRole('link', {name: 'Abstimmung', exact: true}).click();
+    await use(new NavigationToAbstimmung(page));
+  },
+  navigationToFeedback: async({page}, use) => {
+    await page.goto('localhost:4200');
+    await page.getByRole('button', {name: 'Toggle sidenav'}).click();
+    await page.getByRole('link', {name: 'Feedback', exact: true}).click();
+    await use(new NavigationToFeedback(page));
+  },
+  navigationToUmfragen: async({page}, use) => {
+    await page.goto('localhost:4200');
+    await page.getByRole('button', {name: 'Toggle sidenav'}).click();
+    await page.getByRole('link', {name: 'Umfragen', exact: true}).click();
+    await use(new NavigationToUmfragen(page));
+  },
+});
