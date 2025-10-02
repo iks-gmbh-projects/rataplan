e0f1ff535440aad91c7a2a2e5a5c382d3cf22973
vpe <v.peters@iks-gmbh.com>
2025-10-02 10:30:09 +0200

    EXERCISE: Tests for sidenav

--- rataplan-frontend/e2e/homepage.spec.ts
+++ rataplan-frontend/e2e/homepage.spec.ts
@@ -12,3 +12,8 @@ test('change url on "neue Abstimmung"', async({page}) => {
   await page.getByRole('link', {name: 'Neue Abstimmung'}).click();
   await expect(page).toHaveURL(/vote\/create\/general/);
 });
+
+test.describe('sidenav', () => {
+  // TODO Es gibt ein Burgerbutton-Menü in der oberen linken Ecke der Website.
+  //      Schreibe je einen Test für jeden Menüpunkt, der die URL nach der Navigation überprüft.
+});
