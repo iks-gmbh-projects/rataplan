7c7e327acc7c040c947ee15a4b14a3afaea1aff6
vpe <v.peters@iks-gmbh.com>
2025-10-02 14:20:27 +0200

    added classes to fixture

--- rataplan-frontend/e2e/fixtures/navigation/navigation.fixture.ts
+++ rataplan-frontend/e2e/fixtures/navigation/navigation.fixture.ts
@@ -1,5 +1,12 @@
 import { test as base } from '@playwright/test';
+import { NavigationToAbstimmung } from './navigationToAbstimmung';
+import { NavigationToFeedback } from './navigationToFeedback';
+import { NavigationToUmfragen } from './navigationToUmfragen';
 
-type NavigationFixture = {}
+type NavigationFixture = {
+  navigationToAbstimmung: NavigationToAbstimmung;
+  navigationToFeedback: NavigationToFeedback;
+  navigationToUmfragen: NavigationToUmfragen;
+}
 
 export const navigationTest = base.extend<NavigationFixture>({});
