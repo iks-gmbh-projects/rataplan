f3fad45c3d2fb12264bd099484790517706deba8
vpe <v.peters@iks-gmbh.com>
2025-10-08 10:32:50 +0200

    added function to create a survey

--- rataplan-frontend/e2e/fixtures/navigation/navigationToUmfragen.ts
+++ rataplan-frontend/e2e/fixtures/navigation/navigationToUmfragen.ts
@@ -3,4 +3,8 @@ import { Page } from '@playwright/test';
 export class NavigationToUmfragen {
   constructor(public readonly page: Page) {
   }
+  
+  async umfrageErstellen(): Promise<void> {
+    await this.page.getByRole('link', {name: 'Erstellen'}).click();
+  }
 }
