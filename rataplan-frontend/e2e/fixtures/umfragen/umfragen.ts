93a716a0b273682cb92f8f87597511a432e95992
vpe <v.peters@iks-gmbh.com>
2025-10-08 15:07:12 +0200

    edited an added test for filling first Page of survey

--- rataplan-frontend/e2e/fixtures/umfragen/umfragen.ts
+++ rataplan-frontend/e2e/fixtures/umfragen/umfragen.ts
@@ -3,4 +3,15 @@ import { Page } from '@playwright/test';
 export class Umfragen {
   constructor(public readonly page: Page) {
   }
+  
+  async fillFirstPage() {
+    await this.page.getByRole('textbox', {name: 'Umfragename'}).fill('Testname');
+    await this.page.getByRole('textbox', {name: 'Beschreibung'}).fill('Testbeschreibung');
+    await this.page.locator('mat-form-field').filter({hasText: 'Bis'}).getByLabel('Open calendar').click();
+    await this.page.getByRole('button', {name: 'Next month'}).click();
+    await this.page.getByText('21', {exact: true}).click();
+    await this.page.getByText('23').click();
+    await this.page.getByText('30').click();
+    await this.page.getByRole('button').filter({hasText: 'navigate_next'}).click();
+  }
 }
--- rataplan-frontend/e2e/umfragen.spec.ts
+++ rataplan-frontend/e2e/umfragen.spec.ts
@@ -1,7 +1,15 @@
+import { expect, mergeTests } from '@playwright/test';
 import { navigationTest } from './fixtures/navigation/navigation.fixture';
-import { expect } from '@playwright/test';
+import { umfragenTest } from './fixtures/umfragen/umfragen.fixture';
 
-navigationTest('umfragen', async({navigationToUmfragen, page}) => {
+const test = mergeTests(navigationTest, umfragenTest);
+
+test.beforeEach(async({navigationToUmfragen, page}) => {
   await navigationToUmfragen.umfrageErstellen();
   await expect(page).toHaveURL(/survey\/create/);
+});
+
+test('UmfragenFixture Test Startseite', async({umfragen, page}) => {
+  await umfragen.fillFirstPage();
+  await expect(page.getByRole('textbox', {name: 'Frageblocküberschrift'})).toBeVisible();
 });
