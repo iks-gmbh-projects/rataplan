49249b130f2aba18e0b89084e796fef713d89f74
vpe <v.peters@iks-gmbh.com>
2025-10-26 08:10:42 +0100

    generated steps

---
+++ rataplan-frontend/e2e/step_definitions/umfragen.ts
@@ -0,0 +1,17 @@
+import { Given, Then, When } from '@cucumber/cucumber';
+
+Given(/^der Benutzer hat die Umfragen-Seite geöffnet und eine neue Umfrage erstellt$/, async function() {
+
+});
+
+When(/^der Benutzer die erste Seite ausfüllt$/, async function() {
+
+});
+
+When(/^der Benutzer weiter navigiert$/, async function() {
+
+});
+
+Then(/^landet der Benutzer auf der zweiten Seite$/, async function() {
+
+});
