3d14bc2e8f994c864d3183f11ae33984c64918fd
vpe <v.peters@iks-gmbh.com>
2025-10-26 08:54:38 +0100

    edited .gitignore & playwright.config.ts

--- rataplan-frontend/playwright.config.ts
+++ rataplan-frontend/playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { defineBddConfig } from 'playwright-bdd';
 
 /**
  * Read environment variables from file.
@@ -12,7 +13,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: defineBddConfig({
+    paths: ['e2e/features'],
+    require: ['e2e/step_definitions/**/*.ts'],
+    outputDir: '.features-gen',
+  }),
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
