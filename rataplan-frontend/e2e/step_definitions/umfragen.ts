99b1b8e3694e573070f461e6d63448ab6085c848
vpe <v.peters@iks-gmbh.com>
2025-10-26 11:51:37 +0100

    adjust umfragen.ts to use fixtures

--- rataplan-frontend/e2e/step_definitions/umfragen.ts
+++ rataplan-frontend/e2e/step_definitions/umfragen.ts
@@ -4,19 +4,19 @@ import { UmfragenPo } from '../page_objects/umfragen.po';
 
 const {Given, When, Then} = createBdd(test);
 
-Given(/^der Benutzer hat die Umfragen-Seite geöffnet und eine neue Umfrage erstellt$/, async function({page}) {
-  const umfragenPO = new UmfragenPo(page);
-  await umfragenPO.navigateToUmfragenAndCreateNew();
+Given(
+  /^der Benutzer hat die Umfragen-Seite geöffnet und eine neue Umfrage erstellt$/,
+  async function({navigationToUmfragen}) {
+    await navigationToUmfragen.umfrageErstellen();
+  },
+);
+
+When(/^der Benutzer die erste Seite ausfüllt$/, async function({umfragen}) {
+  await umfragen.fillFirstPage();
 });
 
-When(/^der Benutzer die erste Seite ausfüllt$/, async function({page}) {
-  const umfragenPO = new UmfragenPo(page);
-  await umfragenPO.fillFirstPage();
-});
-
-When(/^der Benutzer weiter navigiert$/, async function({page}) {
-  const umfragenPO = new UmfragenPo(page);
-  await umfragenPO.navigateToNextPage();
+When(/^der Benutzer weiter navigiert$/, async function({umfragen}) {
+  await umfragen.clickNextPage();
 });
 
 Then(/^landet der Benutzer auf der zweiten Seite$/, async function({page}) {
